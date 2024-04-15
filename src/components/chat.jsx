41351a1308818a5b75cdabf84aa9222b0488d6d0
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-04-15 11:16:35 +0800

    change react icons to lucide icons

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import React from "react";
-import { IoSearch } from "react-icons/io5";
+import { Search } from "lucide-react";
 import Peopleicon from "./peopleIcon";
 
 const Chat = () => {
   return (
     <div className="bg-blue-00 h-full w-[650px]  flex flex-col gap-5">
       <div className="bg-white h-[60px] min-h-[60px] rounded-3xl flex items-center px-5">
-        <IoSearch size="30" className="text-gray-500" />
+        <Search size="30" className="text-gray-500" />
         <input
           type="text"
           className="h-full w-full bg-transparent  outline-none px-2  text-xl placeholder-gray-500 "
