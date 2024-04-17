9c10f87f5f186c94c02f05010fe4135c78cea640
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-04-17 16:49:56 +0800

    redesign the sidebar component

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -4,12 +4,12 @@ import { LogOut } from "lucide-react";
 
 const Sidebar = () => {
   return (
-    <div className="bg-red-100 h-full w-[133px] bg-gradient-to-l from-[#8f94fb] to-[#4e54c8] rounded-2xl flex flex-col items-center justify-between py-10">
+    <div className=" h-full w-[133px] bg-gradient-to-l from-[#fafafa] to-[#8d93d4] rounded-2xl flex flex-col items-center justify-between py-10">
       <div className="w-[60px] h-[60px] rounded-full overflow-hidden border-2 border-blue-500">
         <Image src="/boys.png" alt="a" width={100} height={100} />
       </div>
       <div>
-        <LogOut size="30" className="text-white" />
+        <LogOut size="30" className="text-[#4e54c8]" />
       </div>
     </div>
   );
