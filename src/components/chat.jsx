1970604e3121b3e5404d7bf8d941b29aeec2a850
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-04-17 16:48:01 +0800

    update the chat component and fetch the all chat api

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,11 +1,19 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { Search } from "lucide-react";
-import Peopleicon from "./peopleIcon";
+import PeopleCard from "./peoplecard";
+import { all } from "axios";
 
-const Chat = () => {
+const Chat = ({ allchat, user, onUserIdChange }) => {
+  const [userId, setUserId] = useState();
+
+  const handeClick = (id) => {
+    setUserId(id);
+    onUserIdChange(userId);
+  };
   return (
-    <div className="bg-blue-00 h-full w-[650px]  flex flex-col gap-5">
+    <div className=" h-full w-[650px]  flex flex-col gap-5">
       <div className="bg-white h-[60px] min-h-[60px] rounded-3xl flex items-center px-5">
         <Search size="30" className="text-gray-500" />
         <input
@@ -16,27 +24,20 @@ const Chat = () => {
       </div>
       <div className="bg-white h-[300px] max-h-[300px] rounded-3xl flex flex-col p-5">
         <h1 className="text-2xl font-semibold">Groups</h1>
-        <div className="flex flex-col gap-3 pt-3 overflow-y-scroll customScroll">
-          <Peopleicon />
-          <Peopleicon />
-          <Peopleicon />
-          <Peopleicon />
-          <Peopleicon />
-        </div>
+        <div className="flex flex-col gap-3 pt-3 overflow-y-scroll customScroll"></div>
       </div>
       <div className="bg-white h-[475px] rounded-3xl flex flex-col p-5">
         <h1 className="text-2xl font-semibold">People</h1>
         <div className="flex flex-col gap-3 pt-3 overflow-y-scroll customScroll">
-          <Peopleicon />
-          <Peopleicon />
-          <Peopleicon />
-          <Peopleicon />
-          <Peopleicon />
-          <Peopleicon />
-          <Peopleicon />
-          <Peopleicon />
-          <Peopleicon />
-          <Peopleicon />
+          {allchat.map((chat) => (
+            <button
+              className=""
+              key={chat._id}
+              onClick={() => handeClick(chat._id)}
+            >
+              <PeopleCard chat={chat} user={user} key={chat._id} />
+            </button>
+          ))}
         </div>
       </div>
     </div>
