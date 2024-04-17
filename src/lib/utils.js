2582e43207b4a39b7c6ce4f444492c6a88cae355
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-04-17 16:53:03 +0800

    make localstorageservice for localstorage interaction

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,42 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export const isBrowser = typeof window !== "undefined";
+// LocalStorageService.js
+export class LocalStorageService {
+  // Get a value from local storage by key
+  static get(key) {
+    if (!isBrowser) return;
+    const value = localStorage.getItem(key);
+    if (value) {
+      try {
+        return JSON.parse(value);
+      } catch (err) {
+        return null;
+      }
+    }
+    return null;
+  }
+
+  // Set a value in local storage by key
+  static set(key, value) {
+    if (!isBrowser) return;
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  // Remove a value from local storage by key
+  static remove(key) {
+    if (!isBrowser) return;
+    localStorage.removeItem(key);
+  }
+
+  // Clear all items from local storage
+  static clear() {
+    if (!isBrowser) return;
+    localStorage.clear();
+  }
 }
