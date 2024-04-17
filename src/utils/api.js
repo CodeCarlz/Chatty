bf410e15c880a837d22150ae073a71e9f7866676
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-04-17 16:51:44 +0800

    make a interceptors for accesstoken

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,25 @@
+// import { LocalStorageService } from "@/lib/utils";
 import axios from "axios";
 
-export default axios.create({
+export const axiosInstance = axios.create({
   // baseURL: "https://chatapp-backend-alpha.vercel.app",
-  baseURL: "http://chatty.evileyedev.in:5000/",
+  baseURL: "http://chatty.evileyedev.in:5000/api/v1/",
   withCredentials: true,
   headers: {
     "Cache-Control": "no-cache",
     "Access-Control-Allow-Origin": "*",
   },
 });
+
+axiosInstance.interceptors.request.use(
+  async function (config) {
+    // Retrieve user token from local storage
+    const accessToken = localStorage.getItem("accessToken");
+    // Set authorization header with bearer token
+    config.headers.Authorization = `Bearer ${accessToken}`;
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
