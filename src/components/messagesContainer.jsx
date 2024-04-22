c0956559fe2c322ccea8597bc2655c5cd6633d42
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-04-22 23:46:26 +0800

    make a error handling closeChat and implement dropdown component

--- src/components/messagesContainer.jsx
+++ src/components/messagesContainer.jsx
@@ -9,13 +9,24 @@ import {
   SendHorizontal,
   Smile,
   Video,
+  cat,
 } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import Message from "./message";
 import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
-const Messages = ({ userId }) => {
+const Messages = ({ userId, closeChat, setCloseChat }) => {
   const { user } = useUser();
   const [allMessages, setAllMessages] = useState([]);
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
@@ -63,100 +74,140 @@ const Messages = ({ userId }) => {
 
   return (
     <div className="bg-white h-full w-full rounded-3xl grid grid-rows-[100px_1fr_90px] overflow-hidden shadow-[0px_4px_5px_2px_#32eed555] ">
-      {/* Profile Container */}
-      <div className=" flex flex-col ">
-        <div className="flex flex-1">
-          <div className=" flex flex-1">
-            <div className=" w-[100px] flex justify-end items-center mr-2">
-              <div className=" h-[60px] w-[60px] rounded-full overflow-hidden">
-                <Image src="/boys.png" height="100" width="100" alt="profile" />
+      {closeChat ? (
+        <>
+          {/* Profile Container */}
+          <div className=" flex flex-col ">
+            <div className="flex flex-1">
+              <div className=" flex flex-1">
+                <div className=" w-[100px] flex justify-end items-center mr-2">
+                  <div className=" h-[60px] w-[60px] rounded-full overflow-hidden">
+                    <Image
+                      src="/boys.png"
+                      height="100"
+                      width="100"
+                      alt="profile"
+                    />
+                  </div>
+                </div>
+                <div className=" flex items-center flex-1">
+                  <div className="">
+                    <h1 className="text-lg font-semibold">Carlz</h1>
+                    <div className="flex gap-2 text-gray-500">
+                      <p>
+                        Online <span>-</span>
+                      </p>
+                      <p>Last seen, 2:02pm</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className=" flex justify-end items-center pr-10 gap-5 flex-1 text-[#4e54c8]">
+                <Phone size={30} />
+                <Video size={35} />
+
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <EllipsisVertical size={30} />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                      <button onClick={() => setCloseChat(false)}>
+                        Close Chat
+                      </button>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>Clear Chat</DropdownMenuItem>
+                    <DropdownMenuItem>Delete Chat</DropdownMenuItem>
+                    <DropdownMenuItem>Block</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
               </div>
             </div>
+            <span className="border-b-2 mx-5 mt-1"></span>
+          </div>
+
+          {/* Chat Container */}
+          <div className="customScroll flex flex-col-reverse overflow-y-auto  gap-7 pb-10">
+            {!loading &&
+              allMessages.map((allMessage) =>
+                allMessage.sender._id != user._id ? (
+                  <div key={allMessage._id}>
+                    <Message allMessage={allMessage} />
+                  </div>
+                ) : (
+                  <div key={allMessage._id}>
+                    <Message allMessage={allMessage} />
+                  </div>
+                )
+              )}
+            {loading && (
+              <div className="grid place-items-center  justify-center  h-full">
+                <Loader2 size={40} className="animate-spin text-primary" />
+              </div>
+            )}
+          </div>
+
+          {/* Send container */}
+          <form
+            className=" flex items-center px-5 gap-5"
+            onSubmit={handleSubmit}
+          >
             <div className=" flex items-center flex-1">
-              <div className="">
-                <h1 className="text-lg font-semibold">Carlz</h1>
-                <div className="flex gap-2 text-gray-500">
-                  <p>
-                    Online <span>-</span>
-                  </p>
-                  <p>Last seen, 2:02pm</p>
+              <div className="flex items-center justify-center flex-1 bg-[#dcecfc] h-[70px] rounded-2xl">
+                <button>
+                  <Paperclip size={30} className="mx-5" />
+                </button>
+                <div className="flex-1  h-full">
+                  <input
+                    type="text"
+                    placeholder="Type Your Message Here..."
+                    className="w-full h-full px-3 outline-none text-lg bg-transparent placeholder-gray-500"
+                    value={sentMessage}
+                    onChange={(e) => setSentMessage(e.target.value)}
+                  />
+                </div>
+
+                <div className="relative">
+                  <button
+                    onClick={(e) => {
+                      setShowEmojiPicker(!showEmojiPicker);
+                      e.preventDefault();
+                    }}
+                  >
+                    <Smile size={30} className="mx-5" />
+                  </button>
+                  <div className="absolute bottom-10 right-12  bg-red-500 transition ease-in  ">
+                    {showEmojiPicker && <EmojiPicker />}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className=" flex justify-end items-center pr-10 gap-5 flex-1 text-[#4e54c8]">
-            <Phone size={30} />
-            <Video size={35} />
-            <EllipsisVertical size={30} />
-          </div>
-        </div>
-        <span className="border-b-2 mx-5 mt-1"></span>
-      </div>
-
-      {/* Chat Container */}
-      <div className="customScroll flex flex-col-reverse overflow-y-auto  gap-7 pb-10">
-        {!loading &&
-          allMessages.map((allMessage) =>
-            allMessage.sender._id != user._id ? (
-              <div key={allMessage._id}>
-                <Message allMessage={allMessage} />
-              </div>
-            ) : (
-              <div key={allMessage._id}>
-                <Message allMessage={allMessage} />
-              </div>
-            )
-          )}
-        {loading && (
-          <div className="grid place-items-center">
-            <Loader2 size={40} className="animate-spin text-primary" />
-          </div>
-        )}
-      </div>
-
-      {/* Send container */}
-      <form className=" flex items-center px-5 gap-5" onSubmit={handleSubmit}>
-        <div className=" flex items-center flex-1">
-          <div className="flex items-center justify-center flex-1 bg-[#dcecfc] h-[70px] rounded-2xl">
-            <button>
-              <Paperclip size={30} className="mx-5" />
+            <button
+              type="submit"
+              className="bg-gradient-to-tr from-[#4e54c8] to-[#b1b4f4] p-3 h-fit rounded-2xl"
+            >
+              {isSending ? (
+                <Loader2 size={40} className="text-white animate-spin" />
+              ) : (
+                <SendHorizontal size={40} className="text-white" />
+              )}
             </button>
-            <div className="flex-1  h-full">
-              <input
-                type="text"
-                placeholder="Type Your Message Here..."
-                className="w-full h-full px-3 outline-none text-lg bg-transparent placeholder-gray-500"
-                value={sentMessage}
-                onChange={(e) => setSentMessage(e.target.value)}
-              />
-            </div>
-
-            <div className="relative">
-              <button
-                onClick={(e) => {
-                  setShowEmojiPicker(!showEmojiPicker);
-                  e.preventDefault();
-                }}
-              >
-                <Smile size={30} className="mx-5" />
-              </button>
-              <div className="absolute bottom-10 right-12  bg-red-500 transition ease-in  ">
-                {showEmojiPicker && <EmojiPicker />}
-              </div>
-            </div>
+          </form>
+        </>
+      ) : (
+        <div className=" h-screen w-full flex flex-col  justify-center items-center gap-10">
+          <div className="h-[400px] w-[400px] rounded-full overflow-hidden">
+            <img src="/boys.png" alt="" className="h-full w-full" />
+          </div>
+          <div className="text-center">
+            <h1 className="text-3xl mb-5 ">Chatty Web</h1>
+            <p>send and recieve messages without keeping your phone online.</p>
+            <p>Use Chatty Web Now</p>
           </div>
         </div>
-        <button
-          type="submit"
-          className="bg-gradient-to-tr from-[#4e54c8] to-[#b1b4f4] p-3 h-fit rounded-2xl"
-        >
-          {isSending ? (
-            <Loader2 size={40} className="text-white animate-spin" />
-          ) : (
-            <SendHorizontal size={40} className="text-white" />
-          )}
-        </button>
-      </form>
+      )}
     </div>
   );
 };
