d0ab0e52d8fa97a0e36fb902c6e6827e9596f555
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-04-22 23:45:15 +0800

    implement the setCloseChat props

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Search } from "lucide-react";
 import PeopleCard from "./peoplecard";
 
-const Chat = ({ allchat, onUserIdChange }) => {
+const Chat = ({ allchat, onUserIdChange, setCloseChat }) => {
   return (
     <div className="  h-full w-[650px]  flex flex-col gap-5">
       <div className="bg-white h-[60px] min-h-[60px] rounded-3xl flex items-center px-5 shadow-[0px_4px_5px_2px_#32eed555] ">
@@ -19,13 +19,21 @@ const Chat = ({ allchat, onUserIdChange }) => {
         <div className="flex flex-col gap-3 pt-3 overflow-y-scroll customScroll"></div>
       </div>
       <div className="bg-white h-[470px] rounded-3xl flex flex-col p-5 shadow-[0px_4px_5px_2px_#32eed555]">
-        <h1 className="text-2xl font-semibold">People</h1>
+        <div className="flex  items-center justify-between">
+          <h1 className="text-2xl font-semibold">People</h1>
+          <button>
+            <Search />
+          </button>
+        </div>
         <div className="flex flex-col gap-3 pt-3 overflow-y-scroll customScroll">
           {allchat.map((chat) => (
             <button
               className=""
               key={chat._id}
-              onClick={() => onUserIdChange(chat._id)}
+              onClick={() => {
+                onUserIdChange(chat._id);
+                setCloseChat(true);
+              }}
             >
               <PeopleCard chat={chat} />
             </button>
