64f559ece5422c7dc98a3a1b56040debf1fa0697
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-04-26 13:16:36 +0800

    update the chat and add allUser props

--- src/components/peoplecard.jsx
+++ src/components/peoplecard.jsx
@@ -1,17 +1,39 @@
 import React from "react";
 import Image from "next/image";
 import { useUser } from "@/context/Usercontext";
+import { cn } from "@/lib/utils";
 
-const PeopleCard = ({ chat }) => {
+const PeopleCard = ({ chat, modal, allUser }) => {
   const { user } = useUser();
 
-  const participantNames = chat.participants
-    .filter((participant) => participant._id !== user._id)
-    .map((participantName) => (
-      <p key={participantName._id}>{participantName.name}</p>
-    ));
+  console.log(chat);
+  // const participantAvatar = chat.participants.find(
+  //   (participant) => participant._id != user._id
+  // );
+  // console.log(participantAvatar)
 
-  const formattedDate = new Date(chat.updatedAt).toLocaleString("en-US", {
+  const chatFiltered = chat?.participants?.find(
+    (participant) => participant._id != user._id
+  );
+
+  console.log(allUser);
+  // const filteredAllUser = allUser?.find((obj) =>
+  //   Object.values(obj).every((value) => value !== undefined)
+  // );
+  // const filterUserData = () => {
+
+  const filterAllUser =
+    allUser === undefined
+      ? console.log("Data is still being fetched. Please try again.")
+      : typeof allUser === "object" && !Array.isArray(allUser)
+      ? allUser.hasOwnProperty("name")
+        ? allUser
+        : console.log('Data does not contain a property named "name".')
+      : console.log("Invalid data format. Expected an object.");
+
+  console.log(filterAllUser);
+
+  const formattedDate = new Date(chat?.updatedAt).toLocaleString("en-US", {
     weekday: "short",
     hour: "numeric",
     minute: "numeric",
@@ -19,19 +41,39 @@ const PeopleCard = ({ chat }) => {
   });
 
   return (
-    <div className=" max-w-[400px] flex  items-center gap-3">
+    <div
+      className={cn(
+        ` flex  items-center gap-3 `,
+        modal ? "bg-gray-300 p-2 rounded-lg" : ""
+      )}
+    >
       <div className=" h-[40px] w-[40px] rounded-full overflow-hidden">
-        <Image src="/boys.png" height="100" width="100" alt="profile" />
+        <Image
+          src={
+            filterAllUser != undefined
+              ? filterAllUser.avatar.url
+              : chatFiltered?.avatar?.url
+          }
+          height="100"
+          width="100"
+          alt="profile"
+        />
       </div>
       <div className=" flex justify-between w-[330px]">
         <div className="text-gray-700  text-start">
-          <h1 className="font-semibold">{participantNames}</h1>
+          <h1 className="font-semibold">
+            {filterAllUser != undefined
+              ? filterAllUser.name
+              : chatFiltered?.name}
+          </h1>
           <p className="text-gray-400 text-ellipsis line-clamp-1 max-w-48">
             {chat?.lastMessage?.content}
           </p>
         </div>
         <div>
-          <p className="text-sm text-gray-500 pt-1">{formattedDate}</p>
+          <p className="text-sm text-gray-500 pt-1">
+            {allUser ? "" : formattedDate}
+          </p>
         </div>
       </div>
     </div>
